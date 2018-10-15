a0b4c6affc588a8d96071d94f8ee0e3db612839a
ronald chaudry <rchaud03@gmail.com>
2018-10-14 20:31:09 -0400

    2030

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+// Friends data
+var friendsArray = [
+	{
+		name: 'Reginald',
+		photo: 'http://adra-matic.com/wp-content/uploads/2014/05/blackthought.jpg',
+		scores: [
+			'1',
+			'4',
+			'5',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ralph',
+		photo: 'http://www.volcreole.com/forum/image-1109.jpg',
+		scores: [
+			'2',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Pedro',
+		photo: 'https://exclaim.ca//images/Raekwon.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5',
+			'5',
+			'3',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Martin',
+		photo: 'http://fr.web.img2.acsta.net/r_1920_1080/medias/nmedia/18/66/05/92/18908300.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	}
+];
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,27 @@
+
 var tableData = require("../data/friends");
+var path = require("path");
 
 module.exports = function(app) {
     app.get("/api/friends", function (req, res) {
-        res.json(friends);          //??
+        res.json(friendsArray);          
     });
-};
 
 app.post("/api/friends", function(req, res) {
+
+    var userResponse = req.body.scores;
+
+    var friendName = '';
+    friendPicture = '';
+
+    for (var i = 0; i < friendsArray.length; i++) {
+        var delta = 0;
+
+        for (var j = 0; j < userResponse.length; j++) {
+            diff += Math.abs(friendsArray[i].scores[j]- userResponse[j]);
+        }
+    }
     friendsArray.push(req.body);
     res.json(true);
 });
+};
